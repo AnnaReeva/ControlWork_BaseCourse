21f63d77b85e07f3445bade83d6d37b60327fae2
Anna Reeva <annree@yandex.ru>
2023-10-20 12:15:47 +0300

    add new array and print method

--- Control_Work/Programm_GetNewTexts/Program.cs
+++ Control_Work/Programm_GetNewTexts/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] texts = new string[4];
+for (int i = 0; i < texts.Length; i++)
+{
+    Console.WriteLine("Enter word: ");
+    string input = Console.ReadLine()!;
+    texts[i] = input;
+}
+
+
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    string gap = "\"";
+    for (int i = 0; i < array.Length; i++)
+    {
+        gap+= array[i].ToString();
+        if (i + 1 != array.Length)
+        {
+            gap += "\", \"";
+        }
+    }
+    Console.Write(gap);
+    Console.Write("\"]");
+    Console.WriteLine();
+}           
+
+PrintArray(texts);
